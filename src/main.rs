70f23342741056fd2ec546b733904c0450825ae4
0mnitron <kucar.andelko@gmail.com>
2024-01-08 01:29:20 +0100

    fixed parallel print

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 use std::time::Instant;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex};
 
-fn unit_propagate(l: i32, cnf_formula: &Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> Vec<Vec<i32>> {
+fn unit_propagate(l: i32, cnf_formula: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     // initialize a new set of clauses
     let mut new_cnf_formula = Vec::new(); 
     // for each clause in cnf_formula
@@ -12,12 +12,9 @@ fn unit_propagate(l: i32, cnf_formula: &Vec<Vec<i32>>, literals: &Arc<RwLock<Vec
             // remove the negation of l from c
             let new_c: Vec<i32> = c.iter().filter(|&x| x != &-l).cloned().collect(); 
             // add the new clause to the new set
-            new_cnf_formula.push(new_c); 
+            new_cnf_formula.push(new_c);
         }
     }
-
-    // Save the literal to the shared vector
-    literals.write().unwrap().push(l);
 
     return new_cnf_formula;
 }
@@ -34,14 +31,16 @@ fn choose_literal(cnf_formula: &Vec<Vec<i32>>) -> Option<i32> {
     return None;
 }
 
-fn dpll_s(cnf_formula: Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> bool {
+fn dpll_s(cnf_formula: Vec<Vec<i32>>, literals: &mut Vec<i32>) -> bool {
     let mut cnf_formula = cnf_formula; 
-    
+
     // unit propagation:
     while cnf_formula.iter().any(|c| c.len() == 1) {
         // get a unit clause
         let l = cnf_formula.iter().find(|c| c.len() == 1).unwrap()[0]; 
-        cnf_formula = unit_propagate(l, &cnf_formula, literals);
+        // Save the literal to the provided vector
+        literals.push(l);
+        cnf_formula = unit_propagate(l, &cnf_formula);
     }
     
     // cnf_formula is empty
@@ -64,7 +63,7 @@ fn dpll_s(cnf_formula: Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> bool 
     return dpll_s(cnf_formula1, literals) || dpll_s(cnf_formula2, literals);
 }
 
-fn dpll_p(cnf_formula: Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> bool {
+fn dpll_p(cnf_formula: Vec<Vec<i32>>, literals: &mut Vec<i32>, has_cleared: &Arc<Mutex<bool>>) -> (bool, Vec<i32>) {
 
     let mut cnf_formula = cnf_formula; 
     
@@ -72,23 +71,20 @@ fn dpll_p(cnf_formula: Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> bool 
     while cnf_formula.iter().any(|c| c.len() == 1) {
         // get a unit clause
         let l = cnf_formula.iter().find(|c| c.len() == 1).unwrap()[0]; 
-        
-        // print literal and thread name
-        // let thread_id = rayon::current_thread_index().unwrap_or(0);
-        // println!("Thread {:?} - Literal {} ", thread_id, l);
-        
-        cnf_formula = unit_propagate(l, &cnf_formula, literals);
+        // Save the literal to the provided vector
+        literals.push(l);
+        cnf_formula = unit_propagate(l, &cnf_formula);
     }
-    
+
     // cnf_formula is empty
     if cnf_formula.is_empty() {
-        return true;
+        return (true, literals.clone());
     }
     // cnf_formula contains an empty clause
     if cnf_formula.contains(&Vec::new()) {
-        return false;
+        return (false, literals.clone());
     }
-    
+
     let l = choose_literal(&cnf_formula).unwrap(); 
 
     let mut cnf_formula1 = cnf_formula.clone(); 
@@ -106,8 +102,35 @@ fn dpll_p(cnf_formula: Vec<Vec<i32>>, literals: &Arc<RwLock<Vec<i32>>>) -> bool 
 
     //return dpll(cnf_formula1) || dpll(cnf_formula2);
 
-    let result = rayon::join(|| dpll_p(cnf_formula1, literals), || dpll_p(cnf_formula2, literals));
-    return result.0 || result.1;
+    // clone literal vector
+    let mut literals_clone1 = literals.clone();
+    let mut literals_clone2 = literals.clone();
+    // clear literal vector
+    literals.clear();
+
+    let result = rayon::join(|| dpll_p(cnf_formula1, &mut literals_clone1, has_cleared), || dpll_p(cnf_formula2, &mut literals_clone2, has_cleared));
+
+    // println!("-1- Literals after parallel execution: ({}) {:?}", literals_clone1.len(), literals_clone1);
+    // println!("-2- Literals after parallel execution: ({}) {:?}", literals_clone2.len(), literals_clone2);
+    
+    // Lock the Mutex in write mode before modifying the flag
+    let mut flag = has_cleared.lock().unwrap();
+    // send literal vector to main
+    if !*flag {
+        *flag = true;
+        literals.extend_from_slice(&literals_clone1);
+        //literals.extend_from_slice(&literals_clone2);
+        drop(flag);
+        return (result.0.0 || result.1.0, literals.clone());
+    } else {
+        if !result.0.1.is_empty() {
+            literals.extend_from_slice(&result.0.1);
+        } else {
+            literals.extend_from_slice(&result.1.1);
+        }
+    }
+
+    return (result.0.0 || result.1.0, literals.clone());
 }
 
 
@@ -156,14 +179,15 @@ fn main() {
     //println!("{:?}", cnf);
 
     // Shared vectors to collect literals during SAT solving (serial and parallel versions)
-    let literals_s = Arc::new(RwLock::new(Vec::new()));
-    let literals_p = Arc::new(RwLock::new(Vec::new()));
+    let mut literals_s = Vec::new();
+    let mut literals_p = Vec::new();
+    // Initialize the flag
+    let has_cleared = Arc::new(Mutex::new(false));
 
     let now = Instant::now();
-    let result1 = dpll_s(cnf.clone(), &literals_s);
+    let result1 = dpll_s(cnf.clone(), &mut literals_s);
     let elapsed = now.elapsed();
     println!("Serial elapsed: {:.2?}", elapsed);
-
 
     if result1 {
         println!("The formula is satisfiable.");
@@ -171,16 +195,17 @@ fn main() {
         println!("The formula is unsatisfiable.");
     }
 
+    
     let now = Instant::now();
-    let result2 = dpll_p(cnf.clone(), &literals_p);
+    let result2 = dpll_p(cnf.clone(), &mut literals_p, &has_cleared);
     let elapsed = now.elapsed();
     println!("Parallel elapsed: {:.2?} with {} threads", elapsed, num_threads);
-    if result2 {
+    if result2.0 {
         println!("The formula is satisfiable.");
     } else {
         println!("The formula is unsatisfiable.");
     }
     
-    println!("\nLiterals after serial execution: {:?}", literals_s.read().unwrap());
-    println!("\nLiterals after parallel execution: {:?}", literals_p.read().unwrap());
+    println!("\nLiterals after serial execution: {:?}", literals_s);
+    println!("\nLiterals after parallel execution: {:?}", literals_p);
 }
